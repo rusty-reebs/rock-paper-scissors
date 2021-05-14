d72ce5131d85f3819c67ac6e372cd9c800b1b137
rusty-reebs <rusty_reebs@tutanota.com>
2021-05-14 10:41:00 +0400

    Added function playRound to play a single round

--- script.js
+++ script.js
@@ -7,5 +7,31 @@ function computerPlay () {
     else {comRandom = "Scissors"};
     return comRandom;
 
-console.log (comRandom);
+// console.log (comRandom);
 }
+
+
+// Function to play a single round of Rock, Paper, Scissors
+
+function playRound (playerSelection, computerSelection) {
+    let result;
+    if (playerSelectionLower == "rock" && computerSelection == "Rock") {result = 'Rock ties Rock rock the world!';}
+    else if (playerSelectionLower == "rock" && computerSelection == "Paper") {result = 'You Lose -- Paper beats Rock too bad!';}
+    else if (playerSelectionLower == "rock" && computerSelection == "Scissors") {result = 'You Win! Rock beats Scissors oh no!';}
+    else if (playerSelectionLower == "scissors" && computerSelection == "Scissors") {console.log('Scissors ties Scissors!');}
+    else if (playerSelectionLower == "scissors" && computerSelection == "Paper") {console.log('You Win! Scissors beats Paper!');}
+    else if (playerSelectionLower == "scissors" && computerSelection == "Rock") {console.log('You Lose -- Rock beats Scissors!');}
+    else if (playerSelectionLower == "paper" && computerSelection == "Paper") {console.log('Paper ties Paper!');}
+    else if (playerSelectionLower == "paper" && computerSelection == "Rock") {console.log('You win! Paper beats Rock!');}
+    else if (playerSelectionLower == "paper" && computerSelection == "Scissors") {console.log('You Lose -- Scissors beats Paper!');}
+    return result;
+}
+
+
+const playerSelection = prompt("Choose Rock, Paper or Scissors");
+playerSelectionLower = playerSelection.toLowerCase();
+const computerSelection = computerPlay();
+
+
+
+// console.log (playRound(playerSelection, computerSelection));
