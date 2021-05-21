1df768fe7ac81cadd85d813437979feec8345314
rusty-reebs <rusty_reebs@tutanota.com>
2021-05-21 09:47:44 +0400

    Current version of JS file for feedback

--- script.js
+++ script.js
@@ -4,24 +4,29 @@ function computerPlay () {
     let comRandom = Math.random();
     if (comRandom <= 0.33) {comRandom = "Rock";}
     else if (comRandom <= 0.67) {comRandom = "Paper";}
-    else {comRandom = "Scissors"};
+    else {comRandom = "Scissors";}
     return comRandom;
+
 }
-// console.log (comRandom);
 
 
 // Function to play a 5-round game
 
 function game () {
     playRound (playerSelectionLower, computerSelection);
+    console.log ("Round 1");
     console.log (playRound(playerSelectionLower, computerSelection));
     playRound (playerSelectionLower, computerSelection);
+    console.log ("Round 2");
     console.log (playRound(playerSelectionLower, computerSelection));
     playRound (playerSelectionLower, computerSelection);
+    console.log ("Round 3");
     console.log (playRound(playerSelectionLower, computerSelection));
     playRound (playerSelectionLower, computerSelection);
+    console.log ("Round 4");
     console.log (playRound(playerSelectionLower, computerSelection));
     playRound (playerSelectionLower, computerSelection);
+    console.log ("Round 5");
     console.log (playRound(playerSelectionLower, computerSelection));
 
 
@@ -32,11 +37,11 @@ function game () {
 
 function playRound (playerSelectionLower, computerSelection) {
     
-    
+
     let result;
-    if (playerSelectionLower == "rock" && computerSelection == "Rock") {result = 'Rock ties Rock rock the world!';}
-    else if (playerSelectionLower == "rock" && computerSelection == "Paper") {result = 'You Lose -- Paper beats Rock too bad!';}
-    else if (playerSelectionLower == "rock" && computerSelection == "Scissors") {result = 'You Win! Rock beats Scissors oh no!';}
+    if (playerSelectionLower == "rock" && computerSelection == "Rock") {result = 'Rock ties Rock!';}
+    else if (playerSelectionLower == "rock" && computerSelection == "Paper") {result = 'You Lose -- Paper beats Rock!';}
+    else if (playerSelectionLower == "rock" && computerSelection == "Scissors") {result = 'You Win! Rock beats Scissors!';}
     else if (playerSelectionLower == "scissors" && computerSelection == "Scissors") {result = 'Scissors ties Scissors!';}
     else if (playerSelectionLower == "scissors" && computerSelection == "Paper") {result = 'You Win! Scissors beats Paper!';}
     else if (playerSelectionLower == "scissors" && computerSelection == "Rock") {result = 'You Lose -- Rock beats Scissors!';}
@@ -49,9 +54,9 @@ function playRound (playerSelectionLower, computerSelection) {
 
 
 
-const playerSelection = prompt("Choose Rock, Paper or Scissors");
+let playerSelection = prompt("Choose Rock, Paper or Scissors");
 playerSelectionLower = playerSelection.toLowerCase();
-const computerSelection = computerPlay();
+computerSelection = computerPlay();
 
 
 console.log (game());
